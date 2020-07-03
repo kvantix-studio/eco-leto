bbb650454d927b370bd02467bb20f74174a1a7ca
Evgeny <salabaikin@mail.ru>
2020-07-03 17:51:11 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,30 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        anchors: ['section-1', 'section-2', 'section-3', 'section-4', 'section-5', 'section-6', 'section-7', 'section-8', 'section-9'],
+        anchors: ['section-1', 'section-2', 'section-3', 'section-4', 'section-5', 'section-6', 'section-7', 'section-8', 'section-9', 'section-10'],
         menu: '#menu',
         animateAnchor: false,
         autoScrolling: true,
         scrollHorizontally: false,
-        paddingTop: '90px',
         navigation: true,
         navigationPosition: 'right',
-        css3:false,
         verticalCentered: false
     });
+});
+
+$('.retail-slider').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: '<div class="retail-slider__prev"></div>',
+  nextArrow: '<div class="retail-slider__next"></div>',
+  responsive: [
+    {
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    }
+  ]
 });
 
 (function() {
