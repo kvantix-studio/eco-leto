a315e54edf0b232f0dc35b8ffc5c9eb540e0c977
Evgeny <salabaikin@mail.ru>
2020-07-07 12:14:49 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -24,6 +24,20 @@ $('.retail-slider').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
       }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 620,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
     }
   ]
 });
